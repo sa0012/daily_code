576c0127d19f11b73ddc8aa17a8daf74e6d4d29f
chenlong <1583479514@qq.com>
2019-09-06 13:13:54 +0800

    update

--- curry/curry.js
+++ curry/curry.js
@@ -1,50 +1,90 @@
-(function () {
+(function() {
   function curry(fn, args) {
     var length = fn.length;
     var args = args || [];
-  
-    console.log(length, args, 'init')
-    return function(){
-        newArgs = args.concat(Array.prototype.slice.call(arguments));
-        console.log(newArgs.length, length, 'length')
-        if (newArgs.length < length) {
-            return curry.call(this,fn,newArgs);
-        }else{
-            return fn.apply(this,newArgs);
-        }
-    }
+
+    console.log(length, args, "init");
+    return function() {
+      newArgs = args.concat(Array.prototype.slice.call(arguments));
+      console.log(newArgs.length, length, "length");
+      if (newArgs.length < length) {
+        return curry.call(this, fn, newArgs);
+      } else {
+        return fn.apply(this, newArgs);
+      }
+    };
   }
-  
+
   function multiFn(a, b, c) {
     return a * b * c;
   }
-  
+
   var multi = curry(multiFn);
-  
+
   // console.log(multi(2)(3)(4));
   // console.log(multi(2, 3, 4));
   // console.log(multi(2)(3, 4));
   // console.log(multi(2, 3)(4));
 })();
 
-(function () {
+(function() {
   // es6版本
   // const curry = (fn, arr = []) => (...args) => (
   //   arg => arg.length === fn.length ? fn(...arg) : curry(fn, arg)
   // )([...arr, ...args]);
 
-  const curry = (fn, arr = []) => (...args) => (
-    arg => arg.length === fn.length ? fn(...arg) : curry(fn, arg)
-  )([...arr, ...args]);
+  const curry = (fn, arr = []) => (...args) =>
+    (arg => (arg.length === fn.length ? fn(...arg) : curry(fn, arg)))([
+      ...arr,
+      ...args
+    ]);
 
   function multiFn(a, b, c) {
     return a * b * c;
   }
-  
+
   var multi = curry(multiFn);
-  
+
   console.log(multi(2)(3)(4));
   // console.log(multi(2, 3, 4));
   // console.log(multi(2)(3, 4));
   // console.log(multi(2, 3)(4));
+})();
+
+(function() {
+  function curry(func, args = []) {
+    var length = func.length;
+
+    return function() {
+      var newArgs = args.concat(Array.prototype.slice.call(arguments));
+      if (newArgs.length < length) {
+        return curry.call(this, func, args);
+      } else {
+        return func.apply(this, newArgs);
+      }
+    };
+  }
+
+  function multiFn(a, b, c) {
+    return a * b * c;
+  }
+
+  var multi = curry(multiFn);
+  console.log(multi(2)(3)(4), "func");
+})();
+
+(function() {
+  const curry = (func, arr) => (...args) => (
+    arg => (arg.length === func.length ? func(...arg) : curry.call(func, arg))
+  )([
+    ...arr,
+    ...args
+  ]);
+
+  function multiFn(a, b, c) {
+    return a * b * c;
+  }
+
+  var multi = curry(multiFn);
+  console.log(multi(1)(2)(3), "es6");
 })();
--- flat/flat.js
+++ flat/flat.js
@@ -1,23 +1,71 @@
-const flatten = function (depth = 1) {
-  let arr = Array.prototype.slice.call(this)
-  if (depth = 0) return [...arr]
+(function() {
+  const flatten = function(depth = 1) {
+    let arr = Array.prototype.slice.call(this);
+    if ((depth = 0)) return [...arr];
 
-  return arr.reduce((prev, cur) => {
-    if (Array.isArray(cur)) {
-      return [...prev, ...flatten.call(cur, depth - 1)]
-    } else {
-      return [...prev, cur]
+    return arr.reduce((prev, cur) => {
+      if (Array.isArray(cur)) {
+        return [...prev, ...flatten.call(cur, depth - 1)];
+      } else {
+        return [...prev, cur];
+      }
+    }, []);
+  };
+
+  Array.prototype.flatten ||
+    Object.defineProperty(Array.prototype, "flatten", {
+      value: flatten,
+      enumerable: false,
+      configurable: true,
+      writable: true
+    });
+
+  let arr = [1, [2, [3, 4, [5, 6]]], 7];
+  let newArr = arr.flatten();
+  console.log(newArr);
+})();
+
+(function() {
+  // 递归方式
+  function flatten(array) {
+    var result = [];
+    for (var i = 0, len = array.length; i < len; i++) {
+      if (Array.isArray(array[i])) {
+        result = result.concat(flatten(array[i]));
+      } else {
+        result.push(array[i]);
+      }
     }
-  }, [])
-}
 
-Array.prototype.flatten || (Object.defineProperty(Array.prototype, 'flatten', {
-  value: flatten,
-  enumerable: false,
-  configurable: true,
-  writable: true
-}));  
+    return result;
+  }
 
-let arr = [1, [2, [3, 4, [5, 6]]], 7];
-let newArr = arr.flatten()
-console.log(newArr)
+  let arr = [1, [2, [3, 4, [5, 6]]], 7];
+  console.log(flatten(arr));
+})();
+
+(function() {
+  // while方式
+  function flatten(array) {
+    while (array.some(item => Array.isArray(item))) {
+      array = [].concat(...array);
+    }
+
+    return array;
+  }
+
+  let arr = [1, [2, [3, 4, [5, 6]]], 7];
+  console.log(flatten(arr));
+})();
+
+(function() {
+  // reduce方式
+  function flatten(array) {
+    return array.reduce((prev, cur, index) => {
+      return prev.concat(Array.isArray(cur) ? flatten(cur) : cur);
+    }, []);
+  }
+
+  let arr = [1, [2, [3, 4, [5, 6]]], 7];
+  console.log(flatten(arr));
+})();
